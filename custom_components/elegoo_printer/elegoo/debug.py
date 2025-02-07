b1bb6e0468e55ca81064fa859c6511ea6f851d33
Daniel Cherubini <daniel@cherubini.casa>
2025-02-07 02:30:19 +0100

    updating debug.py

--- custom_components/elegoo_printer/elegoo/debug.py
+++ custom_components/elegoo_printer/elegoo/debug.py
@@ -1,26 +1,24 @@
 """Debug file for testing elegoo printer."""
 
-import time
+import asyncio
 
 from .elegoo_printer import ElegooPrinterClient
 
 
-def main() -> None:  # noqa: D103
+async def main() -> None:  # noqa: D103
     elegoo_printer = ElegooPrinterClient("10.0.0.212")
     printer = elegoo_printer.discover_printer()
     if printer:
-        connected = elegoo_printer.connect_printer()
+        connected = await elegoo_printer.connect_printer()
         if connected:
             print("Polling Started")  # noqa: T201
-            time.sleep(2)
-            elegoo_printer.get_printer_attributes()
-            time.sleep(10)
-            # while True:
-            #     time.sleep(2)  # noqa: ERA001
-            #     elegoo_printer.get_printer_attributes()  # noqa: ERA001
+            elegoo_printer.set_printer_video_stream(toggle=True)
+            while True:
+                await asyncio.sleep(2)
+                elegoo_printer.get_printer_status()
     else:
         print("No printers discovered.")  # noqa: T201
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
