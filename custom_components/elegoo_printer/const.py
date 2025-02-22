3754c236d54002a5957c38988ef53eefa7011eec
Daniel Cherubini <daniel@cherubini.casa>
2025-02-22 13:31:17 +0100

    Refactoring 2 Electro Boogaloo (#26)
    * more
    
    * fixing things
    
    * formatting
    
    * changing default makefile target
    
    * Update debug.py
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update debug.py
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fixing weird error
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- custom_components/elegoo_printer/const.py
+++ custom_components/elegoo_printer/const.py
@@ -5,4 +5,4 @@ from logging import Logger, getLogger
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "elegoo_printer"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+ATTRIBUTION = "Data provided by https://github.com/danielcherubini/elegoo_printer"
---
+++ debug.py
@@ -0,0 +1,43 @@
+"""Debug file for testing elegoo printer."""
+
+import asyncio
+import os
+import sys
+
+from loguru import logger
+
+from custom_components.elegoo_printer.elegoo_sdcp.const import DEBUG
+from custom_components.elegoo_printer.elegoo_sdcp.elegoo_printer import (
+    ElegooPrinterClient,
+)
+
+LOG_LEVEL = "DEBUG"
+logger.remove()
+logger.add(sys.stdout, colorize=DEBUG, level=LOG_LEVEL)
+
+
+async def main() -> None:
+    """Declare Main function for debugging purposes."""
+    stop_event = asyncio.Event()
+    try:
+        printer_ip = os.getenv("PRINTER_IP", "10.0.0.212")
+        elegoo_printer = ElegooPrinterClient(printer_ip, logger)
+        printer = elegoo_printer.discover_printer()
+        if printer:
+            connected = await elegoo_printer.connect_printer()
+            if connected:
+                logger.debug("Polling Started")
+                await asyncio.sleep(2)
+                elegoo_printer.set_printer_video_stream(toggle=False)
+                elegoo_printer.get_printer_status()
+                elegoo_printer.get_printer_attributes()
+                while not stop_event.is_set():  # noqa: ASYNC110
+                    await asyncio.sleep(2)
+        else:
+            logger.exception("No printers discovered.")
+    except asyncio.CancelledError:
+        stop_event.set()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
