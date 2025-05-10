5c8101dc23508fac4a0458adf49e650ad566af9f
Daniel Cherubini <daniel@cherubini.casa>
2025-05-10 17:10:09 +0200

    Adds Options Flow

--- custom_components/elegoo_printer/config_flow.py
+++ custom_components/elegoo_printer/config_flow.py
@@ -21,6 +21,15 @@ if TYPE_CHECKING:
     from .elegoo_sdcp.models.printer import Printer
 
 
+async def _test_credentials(ip_address: str) -> Printer:
+    """Validate credentials."""
+    elegoo_printer = ElegooPrinterClient(ip_address, LOGGER)
+    printer = elegoo_printer.discover_printer()
+    if printer:
+        return printer
+    raise ElegooPrinterClientGeneralError from Exception("No Printer")
+
+
 class ElegooPrinterClientGeneralError(Exception):
     """Exception For Elegoo Printer."""
 
@@ -29,6 +38,7 @@ class ElegooFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for Elegoo."""
 
     VERSION = 1
+    MINOR_VERSION = 2
 
     async def async_step_user(
         self,
@@ -38,7 +48,7 @@ class ElegooFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         _errors = {}
         if user_input is not None:
             try:
-                printer = await self._test_credentials(
+                printer = await _test_credentials(
                     ip_address=user_input[CONF_IP_ADDRESS],
                 )
             except ElegooPrinterClientWebsocketConnectionError as exception:
@@ -82,10 +92,56 @@ class ElegooFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=_errors,
         )
 
-    async def _test_credentials(self, ip_address: str) -> Printer:
-        """Validate credentials."""
-        elegoo_printer = ElegooPrinterClient(ip_address, LOGGER)
-        printer = elegoo_printer.discover_printer()
-        if printer:
-            return printer
-        raise ElegooPrinterClientGeneralError from Exception("No Printer")
+
+class ElegooOptionsFlowHandler(config_entries.OptionsFlow):
+    """Options flow handler for Elegoo Printer"""
+
+    async def async_setup_init(
+        self, user_input: dict | None = None
+    ) -> config_entries.ConfigFlowResult:
+        """Handle options flow"""
+        _errors = {}
+        if user_input is not None:
+            try:
+                printer = await _test_credentials(
+                    ip_address=user_input[CONF_IP_ADDRESS],
+                )
+            except ElegooPrinterClientWebsocketConnectionError as exception:
+                LOGGER.error(exception)
+                _errors["base"] = "connection"
+            except ElegooPrinterClientWebsocketError as exception:
+                LOGGER.exception(exception)
+                _errors["base"] = "websocket"
+            except (OSError, Exception) as exception:
+                LOGGER.exception(exception)
+                _errors["base"] = "unknown"
+            else:
+                # await self.async_set_unique_id(unique_id=printer.id)
+                # self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=printer.name,
+                    description=printer.name,
+                    data=printer.__dict__,
+                )
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_IP_ADDRESS,
+                        default=(user_input or {}).get(CONF_IP_ADDRESS, vol.UNDEFINED),
+                    ): selector.TextSelector(
+                        selector.TextSelectorConfig(
+                            type=selector.TextSelectorType.TEXT,
+                        ),
+                    ),
+                    vol.Required(
+                        USE_SECONDS,
+                        default=(user_input or {}).get(USE_SECONDS, False),
+                    ): selector.BooleanSelector(
+                        selector.BooleanSelectorConfig(),
+                    ),
+                },
+            ),
+            errors=_errors,
+        )
