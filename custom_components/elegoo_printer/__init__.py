bc781541b8dd41a1b187e0aa3aa815383c611e91
Daniel Cherubini <daniel@cherubini.casa>
2025-06-06 12:26:08 +0200

    Uses Conf Object to init (#47)
    * Uses Conf Object to init
    
    * fixes linting

--- custom_components/elegoo_printer/__init__.py
+++ custom_components/elegoo_printer/__init__.py
@@ -10,11 +10,11 @@ from __future__ import annotations
 from datetime import timedelta
 from typing import TYPE_CHECKING
 
-from homeassistant.const import CONF_IP_ADDRESS, Platform
+from homeassistant.const import Platform
 from homeassistant.loader import async_get_loaded_integration
 
 from .api import ElegooPrinterApiClient
-from .const import CONF_CENTAURI_CARBON, DOMAIN, LOGGER
+from .const import DOMAIN, LOGGER
 from .coordinator import ElegooDataUpdateCoordinator
 from .data import ElegooPrinterData
 
@@ -40,8 +40,7 @@ async def async_setup_entry(
     )
 
     client = await ElegooPrinterApiClient.async_create(
-        ip_address=entry.data[CONF_IP_ADDRESS],
-        centauri_carbon=entry.data[CONF_CENTAURI_CARBON],
+        config=entry.data,
         logger=LOGGER,
     )
 
