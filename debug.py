b4813a62a6337d4a8a60b8690003a29be89328fe
Daniel Cherubini <daniel@cherubini.casa>
2025-06-29 21:14:37 +0200

    updating the debug.py

--- debug.py
+++ debug.py
@@ -8,6 +8,7 @@ from loguru import logger
 
 from custom_components.elegoo_printer.elegoo_sdcp.client import ElegooPrinterClient
 from custom_components.elegoo_printer.elegoo_sdcp.const import DEBUG
+from custom_components.elegoo_printer.elegoo_sdcp.server import ElegooPrinterServer
 
 LOG_LEVEL = "DEBUG"
 PRINTER_IP = os.getenv("PRINTER_IP", "10.0.0.212")
@@ -27,16 +28,14 @@ async def main() -> None:
         elegoo_printer = ElegooPrinterClient(ip_address=PRINTER_IP, logger=logger)
         printer = elegoo_printer.discover_printer(PRINTER_IP)
         if printer:
-            # server = ElegooPrinterServer(printer, logger=logger)
-            # proxied_printer = server.get_printer()
+            server = ElegooPrinterServer(printer, logger=logger)
+            printer = server.get_printer()
             connected = await elegoo_printer.connect_printer(printer)
             if connected:
                 logger.debug("Polling Started")
                 await asyncio.sleep(2)
                 # elegoo_printer.get_printer_attributes()
-                elegoo_printer.set_printer_video_stream(toggle=True)
-                video = elegoo_printer.get_printer_video()
-                await asyncio.sleep(2)
+                video = await elegoo_printer.get_printer_video(toggle=True)
                 if video.status:
                     print(video.status.name)
                 while not stop_event.is_set():  # noqa: ASYNC110
