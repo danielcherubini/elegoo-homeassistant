f52a19bed7130f84df7032ae1db6eb1747690718
Daniel Cherubini <daniel@cherubini.casa>
2025-07-01 21:49:58 +0200

    updated debug and devcontainer

--- debug.py
+++ debug.py
@@ -8,7 +8,6 @@ from loguru import logger
 
 from custom_components.elegoo_printer.elegoo_sdcp.client import ElegooPrinterClient
 from custom_components.elegoo_printer.elegoo_sdcp.const import DEBUG
-from custom_components.elegoo_printer.elegoo_sdcp.server import ElegooPrinterServer
 
 LOG_LEVEL = "DEBUG"
 PRINTER_IP = os.getenv("PRINTER_IP", "10.0.0.212")
@@ -26,18 +25,17 @@ async def main() -> None:
     stop_event = asyncio.Event()
     try:
         elegoo_printer = ElegooPrinterClient(ip_address=PRINTER_IP, logger=logger)
-        printer = elegoo_printer.discover_printer()
+        printer = elegoo_printer.discover_printer(PRINTER_IP)
         if printer:
-            server = ElegooPrinterServer(printer[0], logger=logger)
-            printer = server.get_printer()
-            connected = await elegoo_printer.connect_printer(printer)
+            logger.debug(f"PrinterType: {printer[0].printer_type}")
+            logger.debug(f"Model Reported from Printer: {printer[0].model}")
+            # server = ElegooPrinterServer(printer[0], logger=logger)
+            # printer = server.get_printer()
+            connected = await elegoo_printer.connect_printer(printer[0])
             if connected:
                 logger.debug("Polling Started")
                 await asyncio.sleep(2)
-                elegoo_printer.get_printer_attributes()
-                video = await elegoo_printer.get_printer_video(toggle=True)
-                if video.status:
-                    print(video.status.name)
+                # elegoo_printer.get_printer_attributes()
                 while not stop_event.is_set():  # noqa: ASYNC110
                     # elegoo_printer.get_printer_status()
                     await asyncio.sleep(2)
