1eb8468038fe84234b4009386aebc91be01d964c
Daniel Cherubini <daniel@cherubini.casa>
2025-07-11 10:01:41 +1000

    OK one more

--- custom_components/elegoo_printer/elegoo_sdcp/client.py
+++ custom_components/elegoo_printer/elegoo_sdcp/client.py
@@ -220,9 +220,11 @@ class ElegooPrinterClient:
         if self.printer_data.status.print_info.task_id:
             task_id = self.printer_data.status.print_info.task_id
             current_task = self.printer_data.print_history.get(task_id)
+            self.logger.debug(f"current_task: {current_task}")
             if current_task is not None:
                 return current_task
             else:
+                self.logger.debug("Getting printer task from api")
                 await self.get_printer_task_detail([task_id])
                 await asyncio.sleep(2)  # Give the printer time to respond
                 return self.printer_data.print_history.get(task_id)
