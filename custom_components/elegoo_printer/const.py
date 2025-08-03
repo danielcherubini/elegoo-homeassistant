59232bba7214b9be0f3d1b22796f8a7f274cebbb
Daniel Cherubini <daniel@cherubini.casa>
2025-08-03 14:39:35 +0200

    Random Fixes (#170)
    * Random Fixes
    
    * lint
    
    * fixes

--- custom_components/elegoo_printer/const.py
+++ custom_components/elegoo_printer/const.py
@@ -7,3 +7,19 @@ LOGGER: Logger = getLogger(__package__)
 CONF_PROXY_ENABLED = "proxy_enabled"
 DOMAIN = "elegoo_printer"
 ATTRIBUTION = "Data provided by https://github.com/danielcherubini/elegoo-homeassistant"
+
+# Ports
+WEBSOCKET_PORT = 8888
+DISCOVERY_PORT = 3000
+VIDEO_PORT = 3031
+
+# Endpoints
+VIDEO_ENDPOINT = "video"
+
+# Discovery
+DISCOVERY_MESSAGE = "M99999"
+DEFAULT_BROADCAST_ADDRESS = "<broadcast>"
+DEFAULT_FALLBACK_IP = "8.8.8.8"
+
+# Proxy
+PROXY_HOST = "127.0.0.1"
